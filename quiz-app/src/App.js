5a604152ddceb697ee79442126bed2e5be43d200
Jyotiman <jyotimanlahkar86@gmail.com>
2020-08-23 12:16:12 +0530

    Added starter files

---
+++ quiz-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Welcome to Quiz-App!!!
+      hey, dev hackers !!!
+    </div>
+  );
+}
+
+export default App;
